fb11e265eb968fce7ca741ca649e890b57a2569f
mnazeeri <64224064+mnazeeri@users.noreply.github.com>
2020-05-08 14:38:36 -0500

    Update SecondTaskClass.java

--- src/day_1/SecondTaskClass.java
+++ src/day_1/SecondTaskClass.java
@@ -3,5 +3,7 @@ package day_1;
 public class SecondTaskClass {
     public static void main(String[] args) {
         System.out.println("2nd class of the day");
+        
+        //this class was supposed to be called "Login" but since I was ahead of the teacher I named it differently:Dgit 
     }
 }
